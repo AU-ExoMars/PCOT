6e1d6c01cf08d9be5e50b1f02ddba721c8b52ab8
Jim Finnis <jim.finnis@gmail.com>
2020-09-23 15:43:02 +0100

    default gradients

--- xforms/xformgrad.py
+++ xforms/xformgrad.py
@@ -34,6 +34,27 @@ def applyGradient(img,mask,grad):
     return cp
         
 
+presetGradients = {
+    "topo": [
+            (0.000000, (0.298039,0.000000,1.000000)),
+            (0.111111, (0.000000,0.098039,1.000000)),
+            (0.222222, (0.000000,0.501961,1.000000)),
+            (0.333333, (0.000000,0.898039,1.000000)),
+            (0.444444, (0.000000,1.000000,0.301961)),
+            (0.555556, (0.301961,1.000000,0.000000)),
+            (0.666667, (0.901961,1.000000,0.000000)),
+            (0.777778, (1.000000,1.000000,0.000000)),
+            (0.888889, (1.000000,0.870588,0.349020)),
+            (1.000000, (1.000000,0.878431,0.701961)),
+        ],
+        
+    "grey": [
+        (0, (0,0,0)),
+        (1, (1,1,1)),
+        ],
+}            
+        
+
 @xformtype
 class XformGradient(XFormType):
     """Convert a greyscale image to a colour gradient image for better visibility"""
@@ -47,10 +68,7 @@ class XformGradient(XFormType):
         return TabGradient(n)
         
     def init(self,node):
-        node.gradient = [
-            (0.0, (0,0,0)),
-            (1.0, (1,1,1)),
-        ]
+        node.gradient = presetGradients['topo']
         
     def perform(self,node):
         img = node.getInput(0)
@@ -73,13 +91,25 @@ class TabGradient(ui.tabs.Tab):
     def __init__(self,node):
         super().__init__(ui.mainui,node,'assets/tabgrad.ui')
         self.w.gradient.gradientChanged.connect(self.gradientChanged)
+        self.w.presetCombo.currentIndexChanged.connect(self.loadPreset)
+        for n in presetGradients:
+            self.w.presetCombo.insertItem(1000,n)
         self.onNodeChanged()
+        
+    def loadPreset(self):
+        name = self.w.presetCombo.currentText()
+        print(name)
+        if name in presetGradients:
+            self.w.gradient.setGradient(presetGradients[name])
+            self.w.gradient.update()
+            self.node.gradient = self.w.gradient.gradient()
+            self.node.perform()
+        
             
     def onNodeChanged(self):
         self.w.gradient.setGradient(self.node.gradient)
         self.w.canvas.display(self.node.img)
 
     def gradientChanged(self):
-        print("PERF")
         self.node.gradient = self.w.gradient.gradient()
         self.node.perform()
