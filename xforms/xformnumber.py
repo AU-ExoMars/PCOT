027b430856db7f1b6c01213d89857bc1196852b4
Jim Finnis <jcf12@aber.ac.uk>
2021-03-23 20:04:58 +0000

    Can edit numeric nodes.

--- xforms/xformnumber.py
+++ xforms/xformnumber.py
@@ -4,20 +4,41 @@ from PyQt5.QtCore import Qt
 import ui
 from xform import xformtype, XFormType
 
+validKeys = {Qt.Key_0, Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4, Qt.Key_5, Qt.Key_6, Qt.Key_7, Qt.Key_8, Qt.Key_9,
+             Qt.Key_Period, Qt.Key_Minus,
+             Qt.Key_Delete, Qt.Key_Backspace, Qt.Key_Left, Qt.Key_Right}
+
 
 ## text in middle of main rect and on connections
 class GNumberText(QtWidgets.QGraphicsTextItem):
     def __init__(self, parent, node):
         super().__init__(str(node.val), parent=parent)
         self.setTextInteractionFlags(QtCore.Qt.TextEditorInteraction)
-        self.setTextWidth(100)
+        self.setTextWidth(50)
         self.setTabChangesFocus(True)
         self.node = node
 
+    def focusOutEvent(self, event: QtGui.QFocusEvent) -> None:
+        print("Focus lost")
+        self.setPlainText(str(self.node.val))
+        super().focusOutEvent(event)
+
     def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
-        # Ignore enter key
+        # return means "go!"
         if event.key() == Qt.Key_Return:
+            try:
+                print("BLAP")
+                v = self.toPlainText()
+                self.node.val = float(v)
+                self.clearFocus()
+            except ValueError:
+                ui.error("cannot convert text")
             event.accept()
+        # Ignore non-numeric
+        elif event.key() not in validKeys:
+            event.accept()
+        else:
+            super().keyPressEvent(event)
 
     def setColour(self, col):
         pass
@@ -48,4 +69,3 @@ class XFormNumber(XFormType):
 
     def perform(self, node):
         node.setOutput(0, node.val)
-
