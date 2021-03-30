c41c4c6b1a9ed2f03b14f365ddddf9894ffb515e
Jim Finnis <jcf12@aber.ac.uk>
2021-03-30 13:45:35 +0100

    Added expression parser core, not used yet.

---
+++ expressions/parse.py
@@ -0,0 +1,358 @@
+from io import BytesIO
+from tokenize import tokenize, TokenInfo, NUMBER, NAME, OP, ENCODING, ENDMARKER, NEWLINE
+from typing import List, Any, Optional, Callable, Dict, Tuple
+
+Stack = List[Any]
+
+# will turn on printing
+debug: bool = False
+
+
+class Instruction:
+    def exec(self, stack: Stack):
+        pass
+
+
+class InstNumber(Instruction):
+    val: float
+
+    def __init__(self, v: float):
+        self.val = v
+
+    def exec(self, stack: Stack):
+        stack.append(self.val)
+
+    def __str__(self):
+        return "NUM {}".format(self.val)
+
+
+class InstVar(Instruction):
+    callback: Callable[[], Any]
+
+    def __init__(self, name, callback: Callable[[], Any]):
+        self.callback = callback
+        self.name = name
+
+    def exec(self, stack: Stack):
+        stack.append(self.callback())
+
+    def __str__(self):
+        return "VAR {}".format(self.name)
+
+
+class InstFunc(Instruction):
+    callback: Callable[[List[Any]], Any]
+
+    def __init__(self, name, callback):
+        self.callback = callback
+        self.name = name
+
+    def exec(self, stack: Stack):
+        # actually stack the callback function, don't call it.
+        stack.append(self.callback)
+
+    def __str__(self):
+        return "FUNC {}".format(self.name)
+
+
+class InstOp(Instruction):
+    name: str
+    prefix: bool
+    precedence: int
+
+    def __init__(self, n: str, pre: bool, parser: 'Parser'):
+        if pre and n in parser.unopRegistry:
+            self.precedence, self.callback = parser.unopRegistry[n]
+        elif not pre and n in parser.binopRegistry:
+            self.precedence, self.callback = parser.binopRegistry[n]
+        else:
+            raise ParseException("unknown {} operator: {}".format('prefix' if pre else 'suffix', n))
+        self.name = n
+        self.prefix = pre
+
+    def exec(self, stack: Stack):
+        if self.prefix:
+            stack.append(self.callback(stack.pop()))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(self.callback(a, b))
+
+    def __str__(self):
+        return "OP {} {}".format(self.name, "PRE" if self.prefix else "IN")
+
+
+class InstBracket(InstOp):
+    def __init__(self, parser: 'Parser'):
+        # still need the string argument so InstOp knows which precedence to look up
+        super().__init__('(', False, parser)
+        self.argcount = 0
+
+    def exec(self, stack: Stack):
+        raise Exception("bracket instruction should never be executed")
+
+
+class InstCall(Instruction):
+    def __init__(self, argcount):
+        self.argcount = argcount
+
+    def __str__(self):
+        return "CALL  argcount: {}".format(self.argcount)
+
+    def exec(self, stack: Stack):
+        # semantics here - pop off the args, then pop off the function name (or some other kind of ID!)
+        args = stack[-self.argcount:]
+        # args.reverse()   # is this faster than just popping them in reverse order?
+        # this is really annoying; can't delete multiple items from the stack without slicing and
+        # can't slice because that wouldn't change the original stack.
+        for x in range(0, self.argcount):
+            stack.pop()
+        func = stack.pop()
+        stack.append(func(args))
+
+
+class InstIdent(Instruction):
+    val: str
+
+    def __init__(self, v: str):
+        self.val = v
+
+    def exec(self, stack: Stack):
+        stack.append(self.val)
+
+    def __str__(self):
+        return "STR {}".format(self.val)
+
+
+class ParseException(Exception):
+    def __init__(self, msg: str, t: Optional[TokenInfo] = None):
+        if t is not None:
+            msg = "{}: '{}' at chars {}-{}".format(msg, t.string, t.start[1], t.end[1])
+        super().__init__(msg)
+
+
+def execute(seq: List[Instruction], stack: Stack) -> float:
+    for inst in seq:
+        inst.exec(stack)
+        if debug:
+            print("EXECUTED {}, STACK NOW:".format(inst))
+            for x in stack:
+                print(x)
+    return stack[0]
+
+
+# https://stackoverflow.com/questions/16380234/handling-extra-operators-in-shunting-yard/16392115
+
+class Parser:
+    list: List[TokenInfo]
+    output: List[Instruction]
+    opstack: List[Instruction]  # compile stack
+
+    ## if true, naked identifiers are stacked as strings.
+    nakedIdents: bool
+
+    ## binops are names (e.g. '+') mapped to precedence and two-arg fn which return a value
+    binopRegistry: Dict[str, Tuple[int, Optional[Callable[[Any, Any], Any]]]]
+
+    def registerBinop(self, name: str, precedence: int, fn: Callable[[Any, Any], Any]):
+        self.binopRegistry[name] = (precedence, fn)
+
+    ## unary ops are names mapped to precedence and single arg function which returns a value
+    unopRegistry: Dict[str, Tuple[int, Optional[Callable[[Any], Any]]]]
+
+    def registerUnop(self, name: str, precedence: int, fn: Callable[[Any], Any]):
+        self.unopRegistry[name] = (precedence, fn)
+
+    ## vars are names mapped to argless fns which return a value
+    varRegistry: Dict[str, Callable[[], Any]]
+
+    def registerVar(self, name: str, fn: Callable[[], Any]):
+        self.varRegistry[name] = fn
+
+    ## other functions are names mapped to functions
+    ## which take a list of args and return an arg
+    funcRegistry: Dict[str, Callable[[List[Any]], Any]]
+
+    def registerFunc(self, name: str, fn: Callable[[List[Any]], Any]):
+        self.funcRegistry[name] = fn
+
+    def out(self, inst: Instruction):
+        self.output.append(inst)
+
+    def stackOp(self, op: InstOp):
+        self.opstack.append(op)
+
+    def __init__(self,nakedIdents=False):
+        self.binopRegistry = dict()
+        self.unopRegistry = dict()
+        self.varRegistry = dict()
+        self.funcRegistry = dict()
+        self.toks = []
+
+        self.nakedIdents = nakedIdents
+
+        # preregister the special operators for open bracket
+        self.binopRegistry['('] = (100, None)
+        self.unopRegistry['('] = (100, None)
+
+    def parse(self, s: str):
+        x = BytesIO(s.encode())
+        self.toks = [x for x in (tokenize(x.readline) or []) if x.type != ENCODING
+                     and x.type != ENDMARKER and
+                     x.type != NEWLINE]
+        self.opstack = []
+        self.output = []
+
+        wantOperand = True
+
+        while True:
+            if wantOperand:
+                ######### In this mode, we want an operand next. Otherwise we want an operator.
+                #   read a token. If there are no more tokens, announce an error.
+                t = self.next()
+                if t is None:
+                    raise ParseException("premature end", t)
+                elif t.type == OP:
+                    #  if the token is an prefix operator or an '(':
+                    #    mark it as prefix and push it onto the operator stack
+                    # Note that the bracket case is different from when we meet it in the
+                    # !wantOperand case. It's just a prefix op here, otherwise we'd end up
+                    # generating a CALL when we don't want one.
+                    if t.string == '-' or t.string == '(':
+                        self.stackOp(InstOp(t.string, True, self))
+                        #     goto want_operand (we just loop)
+                    elif t.string == ')':
+                        #     if we meet a ')' it should be a function with no arguments
+                        if len(self.opstack) == 0:  # we need that left bracket
+                            raise ParseException("syntax error : bad no-arg function - no left bracket", t)
+                        op = self.opstack.pop()
+                        if isinstance(op, InstBracket) and not op.prefix:
+                            if op.argcount != 0:
+                                raise ParseException("syntax error : bad no-arg function - has args!", t)
+                            self.out(InstCall(0))
+                            wantOperand = False
+                        else:
+                            raise ParseException("syntax error : no arg-func with no name? ", t)
+                #   if the token is an operand (identifier or variable):
+                #     add it to the output queue
+                #     goto have_operand
+                elif t.type == NUMBER:
+                    self.out(InstNumber(float(t.string)))
+                    wantOperand = False
+                elif t.type == NAME:
+                    if t.string in self.varRegistry:
+                        self.out(InstVar(t.string, self.varRegistry[t.string]))
+                    elif t.string in self.funcRegistry:
+                        fn = self.funcRegistry[t.string]
+                        self.out(InstFunc(t.string, fn))
+                    elif self.nakedIdents:
+                        self.out(InstIdent(t.string))
+                    else:
+                        raise ParseException("unknown variable or function", t)
+                    wantOperand = False
+                else:
+                    #   if the token is anything else, announce an error and stop.
+                    raise ParseException("weird token", t)
+            else:  # wantOperand = false
+                ######### In this mode, we want an operator next. Otherwise we want an operand.
+                #   read a token
+                t = self.next()
+                #   if there are no more tokens:
+                if t is None:
+                    #     pop all operators off the stack, adding each one to the output queue.
+                    while len(self.opstack) > 0:
+                        op = self.opstack.pop()
+                        #     if a `(` is found on the stack, announce an error and stop.
+                        if isinstance(op, InstOp) and op.name == '(':
+                            raise ParseException("syntax error : mismatched bracket left", t)
+                        self.out(op)
+                    return  # ALL DONE
+                #   if the token is a postfix operator:
+                #   could deal with postfix here, but won't.
+
+                # if the token is a ')':
+                if t.type == OP and t.string == ')':
+                    #     while the top of the stack is not '(':
+                    while self.stackTopIsNotLPar():
+                        #       pop an operator off the stack and add it to the output queue
+                        op = self.opstack.pop()
+                        self.out(op)
+                    #     if the stack becomes empty, announce an error and stop.
+                    if len(self.opstack) == 0:
+                        raise ParseException("syntax error : mismatched bracket right", t)
+                    #     if the '(' is marked infix, add a "call" operator to the output queue (*)
+                    #     (using the arg count from the '(' )
+                    op = self.opstack.pop()
+                    if not op.prefix:
+                        if not isinstance(op, InstBracket):
+                            raise ParseException("syntax error : mismatched bracket right 2", t)
+                        self.out(InstCall(op.argcount + 1))
+                    #     pop the '(' off the top of the stack
+                    #     goto have_operand (already there)
+
+                elif t.type == OP and t.string == ',':
+                    #   if the token is a ',':
+                    #     while the top of the stack is not '(':
+                    while self.stackTopIsNotLPar():
+                        #       pop an operator off the stack and add it to the output queue
+                        op = self.opstack.pop()
+                        self.out(op)
+                    # top of stack should now be a '(', which is special - increment the argument count
+                    self.opstack[-1].argcount += 1
+                    #                    self.out(InstComma())  # JCF mod - add comma as actual operator with low precendence
+                    #     if the stack becomes empty, announce an error
+                    if len(self.opstack) == 0:
+                        raise ParseException("syntax error : mismatched bracket left 2", t)
+                    #     goto want_operand
+                    wantOperand = True
+                elif t.type == OP:
+                    #   if the token is an infix operator:
+                    if t.string == '(':
+                        op = InstBracket(self)
+                    else:
+                        op = InstOp(t.string, False, self)
+                    while self.stackTopIsOperatorPoppable(op):
+                        self.out(self.opstack.pop())
+                    self.stackOp(op)
+                    wantOperand = True
+                else:
+                    # token is probably an operand.
+                    raise ParseException("syntax error : unexpected operand", t)
+
+    def stackTopIsNotLPar(self):
+        if len(self.opstack) == 0:
+            return False
+        op = self.opstack[-1]  # peek
+        # must be an operator, and not a left-parenthesis
+        return op.name != '('
+
+    def stackTopIsOperatorPoppable(self, curop):
+        if len(self.opstack) == 0:
+            return False
+        op = self.opstack[-1]  # peek
+        # must not be a left-parenthesis
+        if op.name == '(':
+            return False
+        # operator at stack top must have greater precedence, or the same precedence and token is left-assoc
+        # (which they all are)
+        return op.precedence >= curop.precedence
+
+    def next(self) -> TokenInfo:
+        if self.toksLeft():
+            if debug:
+                print(self.toks[0])
+            return self.toks.pop(0)
+        else:
+            return None
+
+    def rewind(self, tok: TokenInfo):
+        self.toks.insert(0, tok)
+
+    def peek(self) -> TokenInfo:
+        if self.toksLeft():
+            return self.toks[0]
+        else:
+            return None
+
+    def toksLeft(self) -> bool:
+        return len(self.toks) > 0
---
+++ expressions/tests.py
@@ -0,0 +1,182 @@
+from math import sqrt
+
+import parse
+import unittest
+
+variable_1 = 0
+variable_2 = 0
+
+
+def execute(s, nakedIdents=False):
+    p = parse.Parser(nakedIdents=nakedIdents)
+    p.registerFunc('sqrt', lambda a: sqrt(a[0]))
+    p.registerFunc('sqr', lambda a: a[0] * a[0])
+    p.registerFunc('max', lambda a: max(a))
+    p.registerFunc('min', lambda a: min(a))
+    p.registerFunc('firstarg',lambda a: a[0])
+    p.registerFunc('lastarg',lambda a: a[-1])
+    p.registerFunc('noargs',lambda a: 100)
+    p.registerBinop('*', 20, lambda a, b: a * b)
+    p.registerBinop('/', 20, lambda a, b: a / b)
+    p.registerBinop('+', 10, lambda a, b: a + b)
+    p.registerBinop('-', 10, lambda a, b: a - b)
+    p.registerUnop('-', 200, lambda a: -a)
+    p.registerVar('var1', lambda: variable_1)
+    p.registerVar('var2', lambda: variable_2)
+
+    p.parse(s)
+
+    stack = []
+    r = parse.execute(p.output, stack)
+    return r
+
+
+class TestBasic(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(execute('6+6'), 12.0)
+
+    def test_sub(self):
+        self.assertEqual(execute('8-2'), 6.0)
+
+    def test_mul(self):
+        self.assertEqual(execute('8*4'), 32.0)
+
+    def test_div(self):
+        self.assertEqual(execute('16/2'), 8.0)
+
+
+class TestAssociative(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(execute('10*12+4*3'), 132.0)
+
+    def test_2(self):
+        self.assertEqual(execute('10*(12+4)*3'), 480.0)
+
+    def test_3(self):
+        self.assertEqual(execute('(2+2)*(3+3)'), 24.0)
+
+    def test_4(self):
+        self.assertEqual(execute('10/2+9/3'), 8.0)
+
+    def test_5(self):
+        self.assertEqual(execute('10/2-9/3'), 2.0)
+
+    def test_6(self):
+        self.assertEqual(execute('10*(9-2)*3'), 210.0)
+
+    def test_7(self):
+        self.assertEqual(execute('(10-2)*2'), 16.0)
+
+    def test_8(self):
+        self.assertEqual(execute('10-2*2'), 6.0)
+
+
+class TestUnaryMinus(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(execute('-43'), -43)
+
+    def test_2(self):
+        self.assertEqual(execute('-43+10'), -33)
+
+    def test_3(self):
+        self.assertEqual(execute('43*-1'), -43)
+
+    def test_4(self):
+        self.assertEqual(execute('43+-3'), 40)
+
+    def test_5(self):
+        self.assertEqual(execute('-4--3'), -1)
+
+    def test_6(self):
+        self.assertEqual(execute('(-4)'), -4)
+
+    def test_7(self):
+        self.assertEqual(execute('-(-4)'), 4)
+
+    def test_8(self):
+        self.assertEqual(execute('-(2*8)'), -16)
+
+    def test_9(self):
+        self.assertEqual(execute('-(2*-8)'), 16)
+
+
+class TestVariables(unittest.TestCase):
+    def test_1(self):
+        global variable_1
+        variable_1 = 10
+        self.assertEqual(execute('var1'), 10)
+
+    def test_2(self):
+        global variable_1
+        global variable_2
+        variable_1 = 10
+        variable_2 = 21
+        self.assertEqual(execute('var1-var2'), -11)
+
+    def test_3(self):
+        global variable_1
+        global variable_2
+        variable_1 = 10
+        variable_2 = 21
+        self.assertEqual(execute('var1-(var2)'), -11)
+
+    def test_4(self):
+        global variable_1
+        global variable_2
+        variable_1 = 10
+        variable_2 = 21
+        self.assertEqual(execute('var1-(var2/2)'), -0.5)
+
+
+class TestNakedIdents(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(execute('foo', True), 'foo')
+
+    def test_2(self):
+        self.assertRaises(parse.ParseException, lambda: execute('foo'))
+
+    def test_3(self):
+        # not much point to this, but hey.
+        self.assertEqual(execute('blart+foo', True), 'blartfoo')
+
+
+class TestFunctions(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(execute('sqrt(16)'), 4)
+
+    def test_2(self):
+        self.assertEqual(execute('min(45,1,56,12,2)'), 1)
+
+    def test_3(self):
+        self.assertEqual(execute('max(45,1,56,12,2)'), 56)
+
+    def test_4(self):
+        self.assertEqual(execute('firstarg(45,1,56,12,2)'), 45)
+
+    def test_5(self):
+        self.assertEqual(execute('lastarg(45,1,56,12,2)'), 2)
+
+    def test_6(self):
+        self.assertEqual(execute('10+noargs()'), 110)
+
+    def test_7(self):
+        self.assertEqual(execute('max(1,2,4+7,5+1,2*6)'), 12)
+
+    def test_8(self):
+        self.assertEqual(execute('max(1,2,4+7,2*6,5+1)'), 12)
+
+    def test_9(self):
+        self.assertEqual(execute('max(1,2,4+7,min(100,50,200),5+1)'), 50)
+
+    def test_10(self):
+        self.assertEqual(execute('max(0,3,2)+min(7,2,45,3)'), 5)
+
+    def test_11(self):
+        self.assertEqual(execute('max(min(100,50,200),1,2,4+7,5+1)'), 50)
+
+    def test_12(self):
+        self.assertEqual(execute('max(1,2,4+7,5+1,min(100,50,200))'), 50)
+
+
+if __name__ == '__main__':
+    unittest.main()
