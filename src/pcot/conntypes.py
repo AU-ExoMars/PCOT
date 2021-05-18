145395e283841fb571b48c05a0ba66811c265409
Jim Finnis <jcf12@aber.ac.uk>
2021-05-18 10:51:30 +0100

    Qt brush generation for connection types now in connbrushes

--- src/pcot/conntypes.py
+++ src/pcot/conntypes.py
@@ -9,10 +9,6 @@
 # These types are also used by the expression evaluator.
 from typing import Any
 
-from PyQt5 import QtWidgets, uic, QtGui, QtCore
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QColor, QBrush, QLinearGradient
-
 # Here is where new connection types are registered and tested
 # for compatibility: I'm aware that this should really be model-only,
 # but there's UI stuff in here too because (a) I don't want to separate it out and
@@ -47,51 +43,6 @@ DATA = 'data'
 
 def isImage(t: Type):
     return "img" in t
-
-
-## dictionary of name -> brush for connection pad drawing
-
-brushDict = {}
-
-
-## creates a gradient consisting of three colours in quick succession
-# followed by a wide band of another colour. Used to mark connections such as RGB.
-
-def quickGrad(c1, c2, c3, finalC):
-    grad = QLinearGradient(0, 0, 20, 0)
-    grad.setColorAt(0, c1)
-    grad.setColorAt(0.4, c2)
-    grad.setColorAt(0.8, c3)
-    grad.setColorAt(1, finalC)
-    return grad
-
-
-brushDict[ANY] = Qt.red
-brushDict[IMGRGB] = quickGrad(Qt.red, Qt.green, Qt.blue, QColor(50, 50, 50))
-brushDict[IMGGREY] = Qt.gray
-brushDict[IMG] = Qt.blue
-brushDict[ELLIPSE] = Qt.cyan
-brushDict[RECT] = Qt.cyan
-brushDict[DATA] = Qt.darkMagenta
-brushDict[NUMBER] = Qt.darkGreen
-brushDict[VARIANT] = QBrush(Qt.black, Qt.DiagCrossPattern)
-
-## complete list of all types
-types = [x for x in brushDict]
-
-# convert all brushes to actual QBrush objects
-brushDict = {k: QBrush(v) for k, v in brushDict.items()}
-
-
-# add brushes which are already QBrush down here
-
-## get a brush by name or magenta if no brush is found
-def getBrush(typename):
-    if typename in brushDict:
-        return brushDict[typename]
-    else:
-        print("Unknown type ", typename)
-        return QBrush(Qt.magenta)
 
 
 ## are two connectors compatible?
