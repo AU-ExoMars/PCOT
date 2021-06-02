f4f0592b9db24a5568f33df91dfdc027338ebd29
Jim Finnis <jcf12@aber.ac.uk>
2021-06-02 17:07:28 +0100

    error bars in spectra

--- src/pcot/xforms/xformspectrum.py
+++ src/pcot/xforms/xformspectrum.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from matplotlib import cm
 
@@ -9,15 +11,17 @@ from pcot.utils.table import Table
 from pcot.xform import XFormType, xformtype, XFormException
 
 
-# find the mean of all the masked values. Note mask negation!
+# find the mean/sd of all the masked values. Note mask negation!
 def getSpectrum(chanImg, mask):
     if mask is not None:
-        return np.ma.masked_array(data=chanImg, mask=~mask).mean()
+        a = np.ma.masked_array(data=chanImg, mask=~mask)
     else:
-        return chanImg.mean()
+        a = chanImg
+
+    return a.mean(), a.std()
 
 
-# return  wavelength if channel is a single source from a filtered input, else -1.
+# return wavelength if channel is a single source from a filtered input, else -1.
 def wavelength(channelNumber, img):
     source = img.sources[channelNumber]
     if len(source) != 1:
@@ -27,7 +31,37 @@ def wavelength(channelNumber, img):
     return source.getFilter().cwl
 
 
+def processData(table, subimg, legend, wavelengths, spectrum, data, chans, chanlabels, pxct):
+
+    # zip them all together and append to the list for that legend (creating a new list
+    # if there isn't one)
+    if legend not in data:
+        data[legend] = []
+
+    # spectrum is [(mean,sd), (mean,sd)...]
+    means, sds, pixcts = [x[0] for x in spectrum], [x[1] for x in spectrum], [pxct for x in spectrum]
+
+    # data for each region is [ (chan,wave,mean,sd,chanlabel,pixcount), (chan,wave,mean,sd,chanlabel,pixcount)..]
+    # This means pixcount get dupped a lot but it's not a problem
+    data[legend] += list(zip(chans, wavelengths, means, sds, chanlabels, pixcts))
+
+    # add to table
+    table.newRow(legend)
+    table.add("name", legend)
+    table.add("pixels", pxct)
+    for w, s in zip(wavelengths, spectrum):
+        m, sd = s
+        w = int(w)  # Convert wavelength to integer for better table form. Let's hope this doesn't cause problems.
+        table.add("{}mean".format(w), m)
+        table.add("{}sd".format(w), sd)
+
+
 NUMINPUTS = 8
+
+
+ERRORBARMODE_NONE = 0
+ERRORBARMODE_STDERROR = 1
+ERRORBARMODE_STDDEV = 2
 
 
 @xformtype
@@ -38,7 +72,7 @@ class XFormSpectrum(XFormType):
 
     def __init__(self):
         super().__init__("spectrum", "data", "0.0.0")
-        self.autoserialise = ('mode',)
+        self.autoserialise = ('errorbarmode',)
         for i in range(NUMINPUTS):
             self.addInputConnector(str(i), conntypes.IMG, "a single line in the plot")
         self.addOutputConnector("data", conntypes.DATA, "a CSV output (use 'dump' to read it)")
@@ -47,12 +81,15 @@ class XFormSpectrum(XFormType):
         return TabSpectrum(n, w)
 
     def init(self, node):
-        node.mode = 0
+        node.errorbarmode = 0
         node.data = None
 
     def perform(self, node):
         table = Table()
-        data = dict()  # dict of output spectra, key is input name (several inputs might go into one entry if names are same)
+        # dict of output spectra, key is ROI or image name (several inputs might go into one entry if names are same, and one input might
+        # have several ROIs each with a different value)
+        # For each ROI/image there is a lists of tuples, one for each channel : (chanidx, wavelength, mean, sd, name)
+        data = dict()
         for i in range(NUMINPUTS):
             img = node.getInput(i, conntypes.IMG)
             if img is not None:
@@ -67,21 +104,12 @@ class XFormSpectrum(XFormType):
 
                 if len(img.rois) == 0:
                     # no ROIs, do the whole image
-                    legend = "image {}".format(i)
+                    legend = "node input {}".format(i)
                     subimg = img.img
+                    # this returns a tuple for each channel of (mean,sd)
                     spectrum = [getSpectrum(subimg[:, :, i], None) for i in chans]
-                    # zip them all together and append to the list for that legend (creating a new list
-                    # if there isn't one)
-                    if legend not in data:
-                        data[legend] = []
-                    data[legend] += list(zip(chans, wavelengths, spectrum, chanlabels))
-
-                    # add to table
-                    table.newRow(legend)
-                    table.add("name", legend)
-                    table.add("pixels", img.w * img.h)
-                    for w, s in zip(wavelengths, spectrum):
-                        table.add(w, s)
+                    processData(table, subimg, legend, wavelengths,
+                                spectrum, data, chans, chanlabels, img.w * img.h)
 
                 for roi in img.rois:
                     # only include valid ROIs
@@ -91,20 +119,10 @@ class XFormSpectrum(XFormType):
                     # get the ROI bounded image
                     subimg = img.subimage(roi=roi)
                     # now we need to get the mean amplitude of the pixels in each channel in the ROI
+                    # this returns a tuple for each channel of (mean,sd)
                     spectrum = [getSpectrum(subimg.img[:, :, i], subimg.mask) for i in chans]
-
-                    # zip them all together and append to the list for that legend (creating a new list
-                    # if there isn't one)
-                    if legend not in data:
-                        data[legend] = []
-                    data[legend] += list(zip(chans, wavelengths, spectrum, chanlabels))
-
-                    # add to table
-                    table.newRow(legend)
-                    table.add("name", legend)
-                    table.add("pixels", subimg.pixelCount())
-                    for w, s in zip(wavelengths, spectrum):
-                        table.add(w, s)
+                    processData(table, subimg, legend, wavelengths,
+                                spectrum, data, chans, chanlabels, subimg.pixelCount())
 
         # now, for each list in the dict, build a new dict of the lists sorted
         # by wavelength
@@ -116,7 +134,7 @@ class XFormSpectrum(XFormType):
 class TabSpectrum(ui.tabs.Tab):
     def __init__(self, node, w):
         super().__init__(w, node, 'tabspectrum.ui')
-        self.w.mode.currentIndexChanged.connect(self.modeChanged)
+        self.w.errorbarmode.currentIndexChanged.connect(self.errorbarmodeChanged)
         self.w.replot.clicked.connect(self.replot)
         self.w.save.clicked.connect(self.save)
         self.onNodeChanged()
@@ -127,13 +145,24 @@ class TabSpectrum(ui.tabs.Tab):
         self.w.mpl.ax.cla()  # clear any previous plot
         cols = cm.get_cmap('Dark2').colors
 
+        # the dict consists of a list of channel data tuples for each image/roi.
+        colidx = 0
         for legend, x in self.node.data.items():
             try:
-                [chans, wavelengths, spectrum, labels] = list(zip(*x))  # "unzip" idiom
+                [chans, wavelengths, means, sds, labels, pixcounts] = list(zip(*x))  # "unzip" idiom
             except ValueError:
                 raise XFormException("cannot get spectrum - problem with ROIs?")
-            self.w.mpl.ax.plot(wavelengths, spectrum, label=legend)
-            self.w.mpl.ax.scatter(wavelengths, spectrum, c=[wav2RGB(x) for x in wavelengths])
+            col = cols[colidx%len(cols)]
+            self.w.mpl.ax.plot(wavelengths, means, label=legend, c=col)
+            self.w.mpl.ax.scatter(wavelengths, means, c=[wav2RGB(x) for x in wavelengths])
+
+            if self.node.errorbarmode != ERRORBARMODE_NONE:
+                # calculate standard errors from standard deviations
+                stderrs = [std / math.sqrt(pixels) for std, pixels in zip(sds, pixcounts)]
+                self.w.mpl.ax.errorbar(wavelengths, means,
+                                       stderrs if self.node.errorbarmode == ERRORBARMODE_STDERROR else sds,
+                                       ls="None", capsize=4, c=col)
+            colidx += 1
         #            for _, wv, sp, lab in x:
         #                self.w.mpl.ax.annotate(lab, (wv, sp), textcoords='offset points', xytext=(0, 10), ha='center')
         self.w.mpl.ax.legend()
@@ -143,11 +172,12 @@ class TabSpectrum(ui.tabs.Tab):
     def save(self):
         self.w.mpl.save()
 
-    def modeChanged(self, mode):
-        self.node.mode = mode
+    def errorbarmodeChanged(self, mode):
+        self.node.errorbarmode = mode
         self.changed()
 
     def onNodeChanged(self):
         # this is done in replot - the user replots this node manually because it takes
         # a while to run. But we do make the replot button red!
         self.w.replot.setStyleSheet("background-color:rgb(255,100,100)")
+        self.w.errorbarmode.setCurrentIndex(self.node.errorbarmode)
