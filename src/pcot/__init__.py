d93bbf91856bfb691441f6a1f3004791f0664090
Jim Finnis <jcf12@aber.ac.uk>
2022-01-22 12:03:04 +0000

    About box. Version bump.

--- src/pcot/__init__.py
+++ src/pcot/__init__.py
@@ -8,9 +8,15 @@ from pcot.config import getUserName, addMainWindowHook, addExprFuncHook
 from pcot.utils import archive
 from pcot.xforms import *
 
+# get version data, which consists of something like
+#   0.0.0  ISO-DATE RAINBOW CODE NAME
 tmp = pkgutil.get_data('pcot', 'VERSION.txt')
 if tmp is None:
     raise ValueError('cannot find VERSION.txt')
 
-__version__ = tmp.decode('utf-8')
+# this string gets turned into pcot.__fullversion__, while pcot.__version__
+# is just the number part (0.0.0 in the example).
+
+__fullversion__ = tmp.decode('utf-8')
+__version__ = __fullversion__.split(maxsplit=1)[0]
 
