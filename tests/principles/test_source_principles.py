725d35a49fc04bd420a60bdfab978620996be244
Jim Finnis <jcf12@aber.ac.uk>
2022-08-17 12:38:21 +0100

    source extract principle test

--- tests/principles/test_source_principles.py
+++ tests/principles/test_source_principles.py
@@ -31,7 +31,8 @@ def test_envi_sources(envi_img: ImageCube):
 
 
 def test_greyscale_sources(envi_img: ImageCube):
-    # TODO - load ENVI file with bands. Convert to GS. Check sources.
+    """Make sure conversion to greyscale using the grey() function works with regard to sources - this will
+    stand in for certain aspects of the sources principles."""
     assert envi_img.channels == 4
     # I'm going to use the expr "grey" function here. It takes Datum arrays. The second
     # argument specifies whether we are using opencv greyscaling (must be 3 channels).
@@ -43,8 +44,8 @@ def test_greyscale_sources(envi_img: ImageCube):
     from pcot.expressions.eval import funcGrey
     d = funcGrey([Datum(Datum.IMG, envi_img)], [Datum(Datum.NUMBER, 0, nsource)])
     img = d.get(Datum.IMG)
-    assert img.channels == 1        # single channel
-    assert len(img.sources) == 1    # and therefore a single sourceset
+    assert img.channels == 1  # single channel
+    assert len(img.sources) == 1  # and therefore a single sourceset
 
     sources = img.sources[0]
     assert len(sources) == 5  # four channels plus the number determining type
@@ -60,3 +61,25 @@ def test_greyscale_sources(envi_img: ImageCube):
     # and get the filter freqs to make sure they are correct
     freqs = set(x.getFilter().cwl for x in sources if x.getFilter() is not None)
     assert {800, 640, 550, 440} == freqs
+
+
+def test_extract_sources(envi_image_1):
+    """Make sure that an expression of the form a$n works as expected with sources, this time
+    using an actual expr node"""
+    pcot.setup()
+    doc = Document()
+    assert doc.setInputENVI(0, envi_image_1) is None
+
+    inputNode = doc.graph.create("input 0")
+    exprNode = doc.graph.create("expr")
+    exprNode.expr = "a$640"
+    exprNode.connect(0, inputNode, 0)
+
+    doc.changed()
+    img = exprNode.getOutput(0, Datum.IMG)
+    assert img is not None
+    assert len(img.sources) == 1
+    sourceset = img.sources[0]
+    assert len(sourceset) == 1
+    source = sourceset.getOnlyItem()
+    assert source.getFilter().cwl == 640
