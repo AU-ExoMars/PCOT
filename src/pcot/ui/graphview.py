a81f8eca75173d32f782b0acdf4aa4eb67e6916b
Jim Finnis <jcf12@aber.ac.uk>
2022-11-14 11:08:05 +0000

    graphview has a menu, used on rightclick outside a node, which has a "reset view" option to fit all items.

--- src/pcot/ui/graphview.py
+++ src/pcot/ui/graphview.py
@@ -1,27 +1,28 @@
 """This module deals with the widget which displays the graphical scene which
 represents a graph (graphscene).
 """
-from PySide2 import QtWidgets, QtCore
+from PySide2 import QtWidgets, QtCore, QtGui
 from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QMenu
 
 
 class GraphView(QtWidgets.QGraphicsView):
-    """The graphical view widget"""
-    ## constructor, taking the widget parent
+    """The graphical view widget for the graph"""
+
     def __init__(self, parent=None):
         self.window = None
         self._prevMousePos = None
         super().__init__(parent)
 
-    ## sets the window this view is in, and also colours the view
-    # if it is showing a macro.
     def setWindow(self, win, macroWindow):
+        """sets the window this view is in, and also colours the view
+        if it is showing a macro."""
         self.window = win
         if macroWindow:
             self.setStyleSheet("background-color:rgb(255,255,220)")
 
-    ## handle mouse wheel zooming
     def wheelEvent(self, evt):
+        """handle mouse wheel zooming"""
         # Remove possible Anchors
         self.setTransformationAnchor(QtWidgets.QGraphicsView.NoAnchor)
         self.setResizeAnchor(QtWidgets.QGraphicsView.NoAnchor)
@@ -39,17 +40,17 @@ class GraphView(QtWidgets.QGraphicsView):
         # Translate back
         self.translate(-target_viewport_pos.x(), -target_viewport_pos.y())
 
-    ## handle right mouse button panning (when zoomed). This works by
-    # looking at the delta from right mouse button events and applying it
-    # to the scroll bar.
     def mousePressEvent(self, event):
+        """handle right mouse button panning (when zoomed). This works by
+        looking at the delta from right mouse button events and applying it
+        to the scroll bar."""
         if event.button() == Qt.RightButton:
             self._prevMousePos = event.pos()
         else:
             super().mousePressEvent(event)
 
-    ## handle the mouse move event, doing panning if RMB down.
     def mouseMoveEvent(self, event):
+        """handle the mouse move event, doing panning if RMB down."""
         if event.buttons() == Qt.RightButton:
             offset = self._prevMousePos - event.pos()
             self._prevMousePos = event.pos()
@@ -62,19 +63,19 @@ class GraphView(QtWidgets.QGraphicsView):
     # events for handling reception of dragged palette buttons - this
     # interacts with the palette buttons in palette.py
 
-    ## handle a drag move event from the palette
     def dragMoveEvent(self, e):
+        """handle a drag move event from the palette"""
         e.accept()
 
-    ## handle starting a drag
     def dragEnterEvent(self, e):
+        """handle starting a drag"""
         if e.mimeData().hasFormat('data/palette'):
             e.accept()
         else:
             e.ignore()
 
-    ## handle dropping a palette xform
     def dropEvent(self, e):
+        """handle dropping a palette node"""
         bs = e.mimeData().data('data/palette')
         # open the data stream and read the name
         stream = QtCore.QDataStream(bs)
@@ -89,8 +90,8 @@ class GraphView(QtWidgets.QGraphicsView):
         # and build the scene with the new objects
         self.scene().rebuild()
 
-    ## handle key presses
     def keyPressEvent(self, event):
+        """handle key presses"""
         if event.key() == Qt.Key_Delete:
             scene = self.scene()
             scene.mark()
@@ -104,3 +105,17 @@ class GraphView(QtWidgets.QGraphicsView):
             # pass the event into the standard handler,
             # where it will be passed into any items that need it
             super().keyPressEvent(event)
+
+    def contextMenuEvent(self, ev: QtGui.QContextMenuEvent) -> None:
+        super().contextMenuEvent(ev)   # run the super's menu, which will run any item's menu
+        if not ev.isAccepted():        # if the event wasn't accepted, run our menu
+            menu = QMenu()
+            reset = menu.addAction("Reset view")
+            a = menu.exec_(ev.globalPos())
+            if a == reset:
+                self.fitAll()
+
+    def fitAll(self):
+        """Reset the view to fit the entire scene"""
+        self.fitInView(self.scene().itemsBoundingRect(), Qt.KeepAspectRatio)
+
