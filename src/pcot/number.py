65286360bff1bcdeb0608bd8c7831b42f771ab3c
Jim Finnis <jim.finnis@gmail.com>
2023-04-13 19:56:34 +0100

    tests for the uncertainty prop. in binops and modifications to how they work in number.py

--- src/pcot/number.py
+++ src/pcot/number.py
@@ -1,4 +1,7 @@
-"""Dealing with numbers and uncertainties"""
+"""Dealing with numbers and uncertainties
+   Don't add any PCOT bits into this code, because it gets 
+   tested from elsewhere.
+"""
 
 # First, methods to perform the operations. These are separate from Number so we can use them on numpy data too.
 import numpy as np
@@ -25,21 +28,61 @@ def div_unc(a, ua, b, ub):
 
 
 def pow_unc(a, ua, b, ub):
-    """Exponentiation. This is horrible because there are special cases. I'm going to arrange it so that
-    you can only meaningfully do it on a non-negative base."""
+    """Exponentiation. This is horrible because there are special cases when a==0"""
 
-    # This is clever. If a=0 we should
+    ascal = np.isscalar(a)
+    bscal = np.isscalar(b)
+    
+    # If a=0 we should
     #       if b=1:     output UA
-    #       if b<0:     output 100
+    #       if b<0:     output 0 (INVALID)
     #       otherwise:  output 0
+    # The standard calculation performed by the uncertainties module uses the absolute
+    # value of a. I'm not sure this is correct, but I'll do it anyway.
+    
 
-    a = np.abs(a)                       # no negative base allowed!
-    if np.any(a == 0):                  # zero is a special case, deal with those
-        result = np.zeros_like(a)       # get an array of zeros
-        result[b == 1] = ua[b == 1]     # set those places where b=1 to ua.
-        result[b < 0] = 100             # set those places where b<0 to 100.
-        return result
+    if ascal and bscal:
+        if a==0:
+            if b==1:
+                return ua
+            elif b<0:
+                return 0  # these two cases are the same, but it reality the <0 case is invalid.
+            else:
+                return 0
+        # if a!=0 we drop through to the main calculation, after absing a
+        a=abs(a)
+    elif ascal:
+        # a is scalar, b is an array
+        if a==0:
+            result = np.zeros_like(b)
+            result[b==1] = ua           # see below for what we're doing here
+            result[b<0] = 0
+            return result
+        # drop through to main calculation again
+        a=abs(a)
+    else:
+        # a is an array, b is a scalar or an array
+        # for now, just turn b into an array if it isn't one
+        if bscal:
+            b = np.full_like(a,b)
+        
+        a = np.abs(a)   # this isn't ideal!
+        if np.any(a==0):
+            # first, work out where a is 0
+            zeros = a==0
+            # set those values in a to be 1 for now.
+            a[zeros]=1
+            # Perform the core calculation
+            x = a ** (2 * b - 2)
+            y = (a * ub * np.log(a)) ** 2 + (b * ua) ** 2
+            result = np.sqrt(x * y)
+            # set the "zeros" back to zero
+            result[zeros]=0
+            # set to ua where b is 1 and zeros is true
+            np.putmask(result,np.logical_and(zeros,b==1),ua)
+            return result
 
+    # the default calculation
     x = a ** (2 * b - 2)
     y = (a * ub * np.log(a)) ** 2 + (b * ua) ** 2
     return np.sqrt(x * y)
