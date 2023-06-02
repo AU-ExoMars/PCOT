070a511231588fca0408e1d7c089c1eccdf57a18
Jim Finnis <jcf12@aber.ac.uk>
2023-06-02 11:48:40 +0100

    minor tweaks to DQ bits

--- src/pcot/dq.py
+++ src/pcot/dq.py
@@ -22,7 +22,13 @@ def names(bits):
     return "|".join(out)
 
 
+# Note that 'pixel is...' can mean indirectly, so 'pixel is divided by zero' means that the pixel
+# is the result of a chain of calculations that involved a division by zero at some point.
+
 NODATA = reg('nodata', 0)  # Pixel has no data
 NOUNCERTAINTY = reg('nounc', 1)  # Pixel has no uncertainty information.
 SAT = reg('sat', 2)  # Pixel is saturated high
-TEST = reg('test', 4)
+DIVZERO = reg('divzero', 3)     # Pixel is divided by zero
+
+
+TEST = reg('test', 15)
