af5ba60a4efe0745d2aab523f9536dd9027b396d
Jim Finnis <jcf12@aber.ac.uk>
2024-08-19 12:55:47 +0100

    Parameter file change processing, initial work

--- src/pcot/parameters/parameterfile.py
+++ src/pcot/parameters/parameterfile.py
@@ -1,19 +1,52 @@
+"""
+This module handles parameter files - ways of changing multiple parameters, which are stored as TaggedAggregate
+objects.
+
+The workflow is as follows:
+
+* load a parameter file from a path (or string if we are testing)
+* parse the file into a list of changes
+* add a set of tagged aggregates to a dictionary
+* run the "apply" method of the parameter file using that dictionary
+
+This will apply the changes to the tagged aggregates which are in the dictionary - the first element of the
+path is used to look up the TA in the dict, and the rest of the path is used to find the parameter within the TA.
+
+Quite often there will only be a single TA in the dict. We load a single parameter file, and then run every node
+in the graph through it as we load them, with just that node in the dictionary.
+"""
+
+
 from pathlib import Path
 from typing import List, Optional
 import logging
+
+from pcot.parameters.taggedaggregates import TaggedAggregate
 
 logger = logging.getLogger(__name__)
 
 
 class Change:
-    """The parameter file defines changes to parameters."""
+    """The parameter file defines changes to parameters. All parameters have the following:
+    * root_name: the top level of the path is the name of the tagged aggregate in the dict passed to the ParameterFile
+    * path: the path to the parameter within the tagged aggregate, which is the remainder of the path passed to the ctor
+    * key: the key of the parameter within the tagged aggregate, from the last element of the path
+    * line: the line number in the file where the change was defined
+
+    For example, the absolute path foo.bar.baz will be split into root_name = foo, path = [bar], key = baz.
+    """
+
+    root_name: str
+    path: List[str]
+    key: str
+    line: int
 
     def __init__(self, path: List[str], line: int, key: str):
-        self.path = path
+        self.root_name, *self.path = path
         self.key = key
         self.line = line
 
-    def apply(self):
+    def apply(self, data: TaggedAggregate):
         pass
 
 
@@ -24,7 +57,7 @@ class SetValue(Change):
         super().__init__(path, line, key)
         self.value = value
 
-    def apply(self):
+    def apply(self, data: TaggedAggregate):
         logger.info(f"Setting {self.path + [self.key]} to {self.value}")
 
     def __repr__(self):
@@ -37,7 +70,7 @@ class DeleteValue(Change):
     def __init__(self, path: List[str], line: int, key: str):
         super().__init__(path, line, key)
 
-    def apply(self):
+    def apply(self, data: TaggedAggregate):
         logger.info(f"Deleting {self.path + [self.key]}")
 
     def __repr__(self):
@@ -48,13 +81,13 @@ class ParameterFile:
     """Represents a parameter file: a set of changes which can be applied to a graph as it is loaded.
     This class knows nothing about TaggedAggregates, it just deals with the file format."""
 
-    _current: List[str]  # the path to the parent of the last parameter we set
+    _path: List[str]  # the path to the parent of the last parameter we set
     _changes: List[Change]  # the changes to be applied
     path: str  # either the path to the file or "(no path)"
 
     def __init__(self):
         """Create the parameter file object - then use either load (for files) or parse (for strings) to read it"""
-        self._current = []
+        self._path = []
         self._changes = []
         self.path = "(no path)"
 
@@ -63,7 +96,7 @@ class ParameterFile:
         self.path = str(path)
         logger.info(f"Processing parameter file {self.path}")
         with open(path, 'r') as f:
-            s = f.readlines()
+            s = f.read()
             self.parse(s)
         return self
 
@@ -90,9 +123,9 @@ class ParameterFile:
         elements = [x.strip() for x in path_string.split('.')]
 
         # preprocessing - any element that ends in [xxxx] is an index, and should be split into two elements, so
-        # x[y] becomes two elements x,y. These needs to work on multidimension arrays too, so x[y][z] becomes
+        # x[y] becomes two elements x,y. These needs to work on multidimensional arrays too, so x[y][z] becomes
         # x,y,z.
-        path = []
+        path = []           # this is the path we are building by preprocessing the elements to handle indices
         for s in elements:
             if '[' in s:
                 # split the string into parts - so foo[bar] becomes foo,bar] and foo[bar][baz] becomes foo,bar],baz]
@@ -107,8 +140,8 @@ class ParameterFile:
 
         if len(path) == 1:
             # there is no dot, so this is a top-level parameter: the path will be empty.
-            path = []
             key = path_string
+            self._path = []
         else:
             # the last element is the key, the rest is the path.
             key = path.pop()
@@ -119,12 +152,12 @@ class ParameterFile:
                     raise ValueError(
                         f"Invalid parameter: {path_string} - an absolute path must not contain empty elements.")
                 # and set can just set the current path from the path we read.
-                self._current = path
+                self._path = path
             else:
                 # The path is relative. If there is just one empty element, that doesn't change the path - we're working
                 # at the same level. For each extra empty element, we go up one level - at each level ensuring that
                 # we haven't run out of elements.
-                prevCurrent = self._current.copy()
+                prev_path = self._path.copy()
                 # pop empty elements off the start of the path until we hit a non-empty element.
                 empty_ct = 0
                 while len(path) > 0 and path[0] == '':
@@ -133,18 +166,19 @@ class ParameterFile:
                 # ensure there are no empty elements in the rest of the path.
                 if '' in path:
                     raise ValueError(
-                        f"Invalid parameter: {path_string} - relative path must not contain empty elements after the first.")
+                        f"Invalid parameter: {path_string} - relative path must not contain empty elements after the "
+                        f"first.")
                 # now pop the same number of elements off the current path (less one!), making sure there are enough
                 pop_count = empty_ct - 1
-                if pop_count > len(self._current):
+                if pop_count > len(self._path):
                     raise ValueError(
-                        f"Invalid parameter: {path_string} - relative path goes too high for path {prevCurrent}.")
+                        f"Invalid parameter: {path_string} - relative path goes too high for path {prev_path}.")
                 if pop_count > 0:
-                    self._current = self._current[:-pop_count]
+                    self._path = self._path[:-pop_count]
                 # remaining items in path are appended to the current path.
-                self._current.extend(path)
-        logger.info(f"path now is {self._current}, key is {key}")
-        return self._current.copy(), key  # return a copy of the path, not the path itself.
+                self._path.extend(path)
+        logger.info(f"path now is {self._path}, key is {key}")
+        return self._path.copy(), key  # return a copy of the path, not the path itself.
 
     def _process(self, line: str, lineNo: int = 0) -> Optional[Change]:
         """Process each line, generating a Change of some kind"""
@@ -158,8 +192,17 @@ class ParameterFile:
             return DeleteValue(path, lineNo, key)
         else:
             # don't create a change, we're just changing the path
-            self._current, key = self._process_path(line)
+            self._path, key = self._process_path(line)
             return None
+
+    def apply(self, data: dict[str, TaggedAggregate]):
+        """Try to apply the changes to some data objects. Firstly, we look up the tagged aggregate using the first
+        element of the path. If it doesn't exist, we abort silently. Otherwise we use the rest of the path to reference
+        data within the aggregate and change it."""
+
+        for c in self._changes:
+            if c.root_name in data:
+                c.apply(data[c.root_name])
 
     def __str__(self):
         return f"{self.path}: {len(self._changes)} changes"
