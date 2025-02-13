349bfc7554736a4535e80a03f9c55871d6f9c628
Jim Finnis <jim.finnis@gmail.com>
2025-02-13 12:57:33 +0000

    Recents is now a class, rather than a namespace "class"

--- src/pcot/config.py
+++ src/pcot/config.py
@@ -36,8 +36,6 @@ def getUserName():
         return getpass.getuser()
 
 
-data = None
-
 data = configparser.ConfigParser()
 data.read_file(getAssetAsFile('defaults.ini'))
 data.read(['site.cfg', os.path.expanduser('~/.pcot.ini')], encoding='utf_8')
@@ -57,41 +55,41 @@ def getDef(key, fallback='nofallback'):
 
 
 class Recents:
-    paths = deque()
-    COUNT = 5
+    def __init__(self, count):
+        self.paths = deque()
+        self.count = count
 
-    @classmethod
-    def add(cls, path):
+    def add(self, path):
         path = os.path.realpath(path)
-        if path in cls.paths:
-            cls.paths.remove(path)
-        cls.paths.appendleft(path)
-        while len(cls.paths) > cls.COUNT:
-            cls.paths.pop()
+        if path in self.paths:
+            self.paths.remove(path)
+        self.paths.appendleft(path)
+        while len(self.paths) > self.count:
+            self.paths.pop()
 
-    @classmethod
-    def fetch(cls):
-        for i in range(cls.COUNT):
+    def fetch(self,config_data):
+        for i in range(self.count):
             name = "Recent{}".format(i)
             if name in data['Default']:
-                cls.paths.append(data['Default'][name])
+                self.paths.append(config_data['Default'][name])
 
-    @classmethod
-    def store(cls):
-        for i in range(len(cls.paths)):
+    def store(self, config_data):
+        for i in range(len(self.paths)):
             name = "Recent{}".format(i)
-            data['Default'][name] = cls.paths[i]
+            config_data['Default'][name] = self.paths[i]
+
+
+# create and load the recent files singleton
+_recents = Recents(5)
+_recents.fetch(data)
 
 
 def getRecents():
-    return Recents.paths
-
-
-Recents.fetch()
+    return _recents.paths
 
 
 def save():
-    Recents.store()
+    _recents.store(data)
     with open(os.path.expanduser('~/.pcot.ini'), 'w') as f:
         data.write(f)
 
@@ -111,7 +109,7 @@ def getDefaultDir(kind):
 
 def addRecent(fn):
     fn = os.path.realpath(os.path.expanduser(fn))  # just make sure.
-    Recents.add(fn)
+    _recents.add(fn)
     setDefaultDir('pcotfiles', os.path.dirname(fn))
     save()
 
