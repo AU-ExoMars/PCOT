7d13e7c887450d46af42d1e92d8c25df37ac2aed
Jim Finnis <jim.finnis@gmail.com>
2025-06-05 10:32:53 +0100

    lscams can be given a file instead of a camera name. It also shows the source file.

--- src/pcot/subcommands/lscams.py
+++ src/pcot/subcommands/lscams.py
@@ -4,37 +4,46 @@ from pcot.subcommands import subcommand, argument
 @subcommand([
     argument("--long","-l", help="Show long descriptions", action="store_true"),
     argument("--filters","-f", help="Show filters", action="store_true"),
+    argument("--file","-F", help="Read from a PARC file instead of the loaded PCOT cameras", action="store_true"),
     argument("camera", metavar="CAMERA_NAME", help="Camera name", nargs="?")
     ],
     shortdesc="List the available cameras")
 def lscams(args):
     """List the available cameras in the camera directory. If a camera name is given, just show that camera.
     """
-    # first, see if there is a camera directory
-    import pcot.config
     import pcot.cameras
-
-    if pcot.config.getDefaultDir("cameras") is None:
-        print("No camera directory set")
-        return
-    print(f"Camera directory: {pcot.config.getDefaultDir('cameras')}")
-
-    # the system will not have started up fully, so we need to do this.
-    pcot.config.loadCameras()
-
-    if args.camera:
-        camera = pcot.cameras.getCamera(args.camera)
-        if camera is None:
-            print(f"Camera {args.camera} not found")
-        else:
-            show(camera, args)
+    if args.file:
+        # we'll try to load a camera file directly here
+        from pcot.cameras.camdata import CameraData
+        camera = CameraData(args.camera)
+        show(camera, args)
     else:
-        for name in pcot.cameras.getCameraNames():
-            show(pcot.cameras.getCamera(name),args)
+        # we're looking at the loaded cameras, not directly inside a file.
+        # first, see if there is a camera directory
+
+        if pcot.config.getDefaultDir("cameras") is None:
+            print("No camera directory set")
+            return
+        print(f"Camera directory: {pcot.config.getDefaultDir('cameras')}")
+
+        # the system will not have started up fully, so we need to do this.
+        pcot.config.loadCameras()
+
+        if args.camera:
+            camera = pcot.cameras.getCamera(args.camera)
+            if camera is None:
+                print(f"Camera {args.camera} not found")
+            else:
+                show(camera, args)
+        else:
+            for name in pcot.cameras.getCameraNames():
+                show(pcot.cameras.getCamera(name),args)
 
 
 def show(camera, args):
-    print(f"{camera.params.params.name:20}: {camera.params.params.short}")
+    import os.path
+    base_file_name = os.path.basename(camera.fileName)
+    print(f"{camera.params.params.name:20} {base_file_name}: {camera.params.params.short}")
     if args.long:
         print(f"{camera.params.params.description}")
     if args.filters:
