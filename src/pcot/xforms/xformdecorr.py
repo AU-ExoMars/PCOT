553601d8aabe7e400e6df16217776bb756a436ae
Jim Finnis <jim.finnis@gmail.com>
2026-01-10 15:43:39 +0000

    decorr stretch outputs stddevs and eigenvalues

--- src/pcot/xforms/xformdecorr.py
+++ src/pcot/xforms/xformdecorr.py
@@ -6,6 +6,7 @@ from pcot.parameters.taggedaggregates import TaggedDictType
 from pcot.sources import MultiBandSource, SourceSet
 from pcot.ui.tabs import Tab
 from pcot.utils import image
+from pcot.value import Value
 from pcot.xform import xformtype, XFormType, XFormException
 
 
@@ -34,6 +35,8 @@ class XformDecorr(XFormType):
         super().__init__("decorr stretch", "processing", "0.0.0", hasEnable=False)
         self.addInputConnector("rgb", Datum.IMG)
         self.addOutputConnector("rgb", Datum.IMG)
+        self.addOutputConnector("eigs", Datum.NUMBER)
+        self.addOutputConnector("sds", Datum.NUMBER)
         self.params = TaggedDictType(
             stretch=("stretch factor", float, 1.0),
             clip=("percentile out outliers to clip in postprocessing", float, 5.0))
@@ -52,6 +55,8 @@ class XformDecorr(XFormType):
             out = None
             eigvals = None
             stddevs = None
+            eigsout = Datum.null
+            stdsout = Datum.null
         elif img.channels != 3:
             raise XFormException("DATA", "can only decorr stretch images with 3 channels")
         else:
@@ -64,17 +69,27 @@ class XformDecorr(XFormType):
             sources = SourceSet(img.sources.getSources())
 
             # we build the new DQ by OR-ing all the bands' bits together
-            dq = np.bitwise_or.reduce(subimage.dq, axis=2)
+            dqval = np.bitwise_or.reduce(subimage.dq, axis=2)
             # and then using that for all new channels
-            dq = image.imgmerge((dq, dq, dq))
+            dq = image.imgmerge((dqval, dqval, dqval))
             dq |= pcot.dq.NOUNCERTAINTY     # and we've lost all uncertainty data
 
             out = img.modifyWithSub(subimage, newimg, sources=MultiBandSource([sources, sources, sources]), dqv=dq).setMapping(node.mapping)
             out = Datum(Datum.IMG, out)
 
+            # build the unc and dq values for the vector outputs, completely flattening the DQs into a single int
+            uncs = np.full(eigvals.shape, 0.0, dtype=np.float32)
+            dqval = np.bitwise_or.reduce(dqval, axis=None)
+            dqs = np.full(eigvals.shape, dqval, dtype=np.uint16)
+            # and the values themselves
+            eigsout = Datum(Datum.NUMBER, Value(eigvals, uncs, dqs), sources=sources)
+            stdsout = Datum(Datum.NUMBER, Value(stddevs, uncs, dqs), sources=sources)
+
         node.eigenvals = eigvals
         node.stddevs = stddevs
         node.setOutput(0, out)
+        node.setOutput(1, eigsout)
+        node.setOutput(2, stdsout)
 
 class TabDecorr(Tab):
     def __init__(self, node, w):
@@ -98,9 +113,16 @@ class TabDecorr(Tab):
 
         self.w.stretchSpin.setValue(p.stretch)
         self.w.clipSpin.setValue(p.clip)
+        sf = pcot.config.getint("sigfigs")
 
-        self.w.stdDevsText.setText(str(self.node.stddevs))
-        self.w.eigenValsText.setText(str(self.node.eigenvals))
+        if self.node.stddevs is None:
+            self.w.stdDevsText.setText("No data")
+            self.w.eigenValsText.setText("No data")
+        else:
+            s = [f"{i}: {round(self.node.stddevs[i],sf)}" for i in range(len(self.node.stddevs))]
+            self.w.stdDevsText.setText("\n".join(s))
+            s = [f"{i}: {round(self.node.eigenvals[i],sf)}" for i in range(len(self.node.stddevs))]
+            self.w.eigenValsText.setText("\n".join(s))
 
 
         self.w.canvas.display(self.node.getOutput(0))
